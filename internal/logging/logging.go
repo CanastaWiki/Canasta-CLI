015533c64bb2cc0dd3acc803c6da83be28d679e2
Amal Paul <amalpaulofficial@gmail.com>
2022-06-22 17:47:26 +0530

    Add Basic functions for logging installation details

--- internal/logging/logging.go
+++ internal/logging/logging.go
@@ -1,22 +1,97 @@
-package main
+package logging
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"os"
 )
 
+type CanastaInstallation struct {
+	Id, Path, Orchestrator string
+}
+
+type Canasta struct {
+	Installations map[string]CanastaInstallation
+}
+
 var (
-	directory string
-	confFile  string
+	directory             string
+	confFile              string
+	existingInstallations Canasta
 )
+
+func Exists(canastaId string) bool {
+	err := read(&existingInstallations)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return existingInstallations.Installations[canastaId].Id != ""
+}
+
+func ListAll() {
+	err := read(&existingInstallations)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Canasta ID\tInstallation Path\tOrchestrator\n\n")
+	for _, installation := range existingInstallations.Installations {
+		fmt.Printf("%s\t%s\t%s\n", installation.Id, installation.Path, installation.Orchestrator)
+	}
+}
+
+func GetDetails(canastaId string) (CanastaInstallation, error) {
+	if Exists(canastaId) {
+		return existingInstallations.Installations[canastaId], nil
+	}
+	return CanastaInstallation{}, fmt.Errorf("Canasta Installation with the ID doesn't exist")
+}
+
+func Add(details CanastaInstallation) error {
+	if Exists(details.Id) {
+		return fmt.Errorf("Canasta ID is already used for another installation")
+	} else {
+		existingInstallations.Installations[details.Id] = details
+	}
+	err := write(existingInstallations)
+	return err
+}
+
+func Delete(canastaID string) error {
+	if Exists(canastaID) {
+		delete(existingInstallations.Installations, canastaID)
+	} else {
+		return fmt.Errorf("Canasta Installation with the ID doesn't exist")
+	}
+	err := write(existingInstallations)
+	return err
+}
+
+func write(details Canasta) error {
+
+	file, err := json.MarshalIndent(details, "", "	")
+	if err != nil {
+		return err
+	}
+	return ioutil.WriteFile(confFile, file, 0644)
+}
+
+func read(details *Canasta) error {
+	data, err := os.ReadFile(confFile)
+	if err != nil {
+		return err
+	}
+	err = json.Unmarshal(data, details)
+	return err
+}
 
 func init() {
 
 	log.SetFlags(0)
 	log.SetPrefix("Error: ")
 	directory = "/etc/canasta"
-	confFile = directory + "/confFile.js"
+	confFile = directory + "/conf.js"
 
 	//Checks for the conf.js file
 	_, err := os.Stat(confFile)
@@ -39,13 +114,4 @@ func init() {
 			log.Fatal(err)
 		}
 	}
-}
-
-func write() error {
-
-	return nil
-}
-
-func main() {
-
 }
