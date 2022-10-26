3afb995f770a20ca7d201d08c2041ef787ff8331
Jeffrey Wang <jeffw16@users.noreply.github.com>
2022-10-26 08:43:28 +0200

    Add build specification for Linux amd64

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@
 # Build script to populate the version information in Canasta.
 # See https://www.digitalocean.com/community/tutorials/using-ldflags-to-set-version-information-for-go-applications
 
-go build -ldflags="-X 'github.com/CanastaWiki/Canasta-CLI-Go/cmd/version.sha1=$(git rev-parse --short HEAD)'
+env GOOS=linux GOARCH=amd64 go build -ldflags="-X 'github.com/CanastaWiki/Canasta-CLI-Go/cmd/version.sha1=$(git rev-parse --short HEAD)'
     -X 'github.com/CanastaWiki/Canasta-CLI-Go/cmd/version.buildTime=$(date +'%Y-%m-%d %T')'"
