4d0c7406a01dc85de99645bb217815d9eb00d5f1
Yaron Koren <yaron57@gmail.com>
2023-02-19 23:24:06 -0500

    Improve output messages and comments in install.sh (#65)

--- install.sh
+++ install.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
 # Canasta CLI installer
-# Requirements Docker Engine 18.06.0+ and DockerCompose 
 
-echo "Downloading Canasta CLI latest release"
+# This script downloads and installs the Canasta command-line interface (CLI), which is
+# an executable called "canasta".
+# It also checks for the presence of Git, Docker and Docker Compose, and displays a
+# warning to the user if they are not correctly installed, although it installs the CLI
+# regardless.
+
+echo "Downloading Canasta CLI latest release..."
 canastaURL="https://github.com/CanastaWiki/Canasta-CLI/releases/latest/download/canasta"
 # The show-progress param was added to wget in version 1.16 (October 2014).
 wgetOptions=$(wget --help)
@@ -14,20 +19,18 @@ else
   wget -q $canastaURL
 fi
 
-echo "Installing Canasta CLI"
+echo "Download was successful; now installing Canasta CLI."
 chmod u=rwx,g=xr,o=x canasta
 sudo mv canasta /usr/local/bin/canasta
 
-# Testing git if installed else tell to install it
+# Check if Git is installed; exit if not.
 git --version 2>&1 >/dev/null
-
 GIT_IS_AVAILABLE=$?
-
 if [ $GIT_IS_AVAILABLE -ne 0 ]; 
-then echo "Git was not found, please install before continuing.";
+then echo "Git was not found, please install it before continuing.";
      exit; 
 else
-     echo "Git was found on the system"
+     echo "Checking for presence of Git... found."
 fi
 
 loc=$(which docker)
@@ -36,7 +39,7 @@ then
     echo "Docker is not installed; please follow the guide at https://docs.docker.com/engine/install/ to install it."
 elif [ -x $loc ]
 then
-    echo "Docker is already installed."
+    echo "Checking for presence of Docker... found."
 else
     echo "Docker appears to be installed at $loc but is not executable; please check permissions."
 fi
@@ -47,7 +50,7 @@ then
     echo "Docker Compose is not installed; please follow the guide at https://docs.docker.com/compose/install/ to install it."
 elif [ -x $loc ]
 then
-    echo "Docker Compose is already installed."
+    echo "Checking for presence of Docker Compose... found."
 else
     echo "Docker Compose appears to be installed at $loc but is not executable; please check permissions."
 fi
