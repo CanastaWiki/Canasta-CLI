cc69d7e70f9d1d6b10d80b38cf885d281775c23f
Lakshy S. <suntiwari3495@gmail.com>
2026-03-02 18:35:50 +0000

    fix/refactor: add edge cases for empty map, partial S3 vars, and RESTIC_REPOSITORY empty string fallthrough
    Signed-off-by: Lakshy S. <suntiwari3495@gmail.com>

--- cmd/backup/backup_test.go
+++ cmd/backup/backup_test.go
@@ -204,6 +204,7 @@ func TestGetRepoURL(t *testing.T) {
 		{
 			name: "RESTIC_REPO is set",
 			env: map[string]string{
+				"RESTIC_REPOSITORY": "",
 				"RESTIC_REPO":   "rest:http://repo.example/repo",
 				"AWS_S3_API":    "https://s3.example",
 				"AWS_S3_BUCKET": "bucket",
@@ -219,13 +220,32 @@ func TestGetRepoURL(t *testing.T) {
 			want: "s3:https://s3.example/bucket",
 		},
 		{
-			name: "all vars empty",
+			name: "only AWS_S3_API is set",
+			env: map[string]string{
+				"AWS_S3_API": "https://s3.example",
+			},
+			want: "s3:https://s3.example/",
+		},
+		{
+			name: "only AWS_S3_BUCKET is set",
+			env: map[string]string{
+				"AWS_S3_BUCKET": "bucket",
+			},
+			want: "s3:/bucket",
+		},
+		{
+			name: "all vars empty strings",
 			env: map[string]string{
 				"RESTIC_REPOSITORY": "",
 				"RESTIC_REPO":       "",
 				"AWS_S3_API":        "",
 				"AWS_S3_BUCKET":     "",
 			},
+			want: "s3:/",
+		},
+		{
+			name: "all vars absent",
+			env:  map[string]string{},
 			want: "s3:/",
 		},
 	} {
