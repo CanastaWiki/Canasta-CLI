2049ce69757b2af0233db74df01e25e3635cf4b3
Cindy Cicalese <cindom@gmail.com>
2026-03-22 18:18:50 -0400

    Fix gofmt alignment in userEditablePaths

--- internal/canasta/canasta.go
+++ internal/canasta/canasta.go
@@ -65,8 +65,8 @@ var userEditablePaths = map[string]bool{
 	"config/Caddyfile.site":             true,
 	"config/Caddyfile.global":           true,
 	"config/settings/global/Vector.php": true,
-	"config/settings/global/CanastaFooterIcon.php":  true,
-	"config/settings/global/DefaultSettings.php":    true,
+	"config/settings/global/CanastaFooterIcon.php": true,
+	"config/settings/global/DefaultSettings.php":   true,
 }
 
 // noClobberPaths lists informational template files that are created once
